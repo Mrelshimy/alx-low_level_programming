bd3da7b951a7be885fa0466dc8a9d8a2f3fe80c4
Mrelshimy <mraafat.elsayed@gmail.com>
2023-08-11 08:37:49 -0700

    Adding file 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription: A C program for printing all possible combinations
+ * of single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 9)
+	{
+		putchar('0' + n);
+		putchar(',');
+		putchar(' ');
+		n++;
+	}
+
+	putchar('0' + n);
+	return (0);
+}
