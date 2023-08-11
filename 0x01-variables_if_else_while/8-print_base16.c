726d08fc2e0502712bf3ea821ec906bc70b5e816
Mrelshimy <mraafat.elsayed@gmail.com>
2023-08-11 07:58:26 -0700

    Adding file 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription: A C program for printing alphabet
+ * in lowercase in reverse using putchar()
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 102)
+	{
+		putchar(n);
+		
+		if (n == 57)
+			n += 39;
+		n++;
+	}
+
+	putchar('\n');
+	return (0);
+}
