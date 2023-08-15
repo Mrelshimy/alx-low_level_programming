f0ae4834b0a6b84608fd8fe260286e82f276ab7b
Mrelshimy <mraafat.elsayed@gmail.com>
2023-08-15 12:50:21 -0700

    Updating file 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,16 +12,20 @@
 int main(void)
 {
 	int i;
-	int f = 0;
-	int g = 1;
-	int h = 1;
+	unsigned long f = 0;
+	unsigned long g = 1;
+	unsigned long h = 1;
 
 	for (i = 1 ; i <= 50 ; i++)
 	{
-		printf("%d, ", h);
+		printf("%lu", h);
 		f = g;
 		g = h;
 		h = f + g;
+		if (i == 50)
+			printf("\n");
+		else
+			printf(", ");
 	}
 	printf("\n");
 
