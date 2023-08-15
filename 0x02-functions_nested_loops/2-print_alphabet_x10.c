ee7bc99a55ce3dc8d066fc97c03ecbfd7f305f70
Mrelshimy <mraafat.elsayed@gmail.com>
2023-08-15 02:54:33 -0700

    upadting file 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a C file for printing alphabet in lower case
+ * 10 times usnig _putchar()
+ */
+
+void print_alphabet_x10(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		char i = 'a';
+
+		while (i <= 'z')
+		{
+			_putchar(i);
+			i++;
+		}
+		x += 1;
+	}
+	_putchar('\n');
+}
