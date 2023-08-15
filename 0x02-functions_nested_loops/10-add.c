e71d8f5ff3695cfccd33530673ce50eb2b65131f
Mrelshimy <mraafat.elsayed@gmail.com>
2023-08-15 05:20:24 -0700

    Adding file 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  add - Function for ading 2 integers
+ *@a : 1st integer
+ *@b : 2nd integer
+ *
+ * Discription : a C file for adding 2 integers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	a = a + b;
+	return (a);
+}
