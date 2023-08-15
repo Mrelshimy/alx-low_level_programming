0681d1ba70945634a6bd0561daf04345c5f06aff
Mrelshimy <mraafat.elsayed@gmail.com>
2023-08-15 00:25:23 -0700

    Adding 0x02 project directory + README

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Discription : a C file for printing _putchar usnig putchar()
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c[] = "_putchar";
+	int i = 0;
+
+	for (i = 0 ; i <= 7 ; i++)
+	{
+		_putchar(c[i]);
+	}
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - Entry point
+ *@c : Character to be printed
+ *
+ * Discription: A C program for creating a header file
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _putchar (char c);
+
+#endif
