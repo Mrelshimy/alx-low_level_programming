e95cc8f893e08e8d07797de0ef939b692e8bd92d
Mrelshimy <mraafat.elsayed@gmail.com>
2023-08-15 12:34:08 -0700

    Adding file 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *  main  - Entry point
+ *
+ * Discription : a C file for printing first 5- fibonacci numbers
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int f = 0;
+	int g = 1;
+	int h;
+
+	for (i = 1 ; i <= 50 ; i++)
+	{
+		printf("%d, ", h);
+		f = g;
+		g = h;
+		h + f + g;
+	}
+	printf("\n");
+
+	return (0);
+}
