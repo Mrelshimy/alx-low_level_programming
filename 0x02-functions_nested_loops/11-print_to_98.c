6a08dfe0083def3a0110e469ca00e1eb8d3c1041
Mrelshimy <mraafat.elsayed@gmail.com>
2023-08-15 06:06:20 -0700

    Updating file 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,26 +14,17 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 		{
-			_putchar(n + '0');
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ",n);
 			n--;
 		}
-		_putchar(n + '0');
 	}
-	else if (n < 98)
+	else (n < 98)
 	{
 		while (n < 98)
 		{
-			_putchar(n + '0');
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ",n);
 			n++;
 		}
-		_putchar(n + '0');
 	}
-	else
-	{
-		_putchar(n + '0');
-	}
+	printf("98\n");
 }
