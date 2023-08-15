428f3859e7410e38a3a8e66b214951ac485e4a2a
Mrelshimy <mraafat.elsayed@gmail.com>
2023-08-15 05:38:49 -0700

    Adding file 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *  print_to_98 - Function for printing from int to 98
+ *@n : integer
+ *
+ * Discription : a C file for printing from int to 98
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (n > 98; n; n--)
+		{
+			_putchar(n + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar(98 + '0')
+	}
+	else if (n < 98)
+	{
+		for (n < 98; n; n++)
+		{
+			_putchar(n + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar(98 + '0');
+	}
+	else
+	{
+		_putchar(98 + '0');
+
+}
