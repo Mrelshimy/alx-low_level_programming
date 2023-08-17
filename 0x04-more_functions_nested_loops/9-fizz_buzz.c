fb187ef8914491c854af3f52c1b1fb96be86556a
Mrelshimy <mraafat.elsayed@gmail.com>
2023-08-17 11:04:40 -0700

    Updating file 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,13 +14,17 @@ int main(void)
 
 	for (n = 1 ; n <= 100 ; n++)
 	{
-		if (n % 3 == 0)
+		if (n % 3 == 0 && n % 5 == 0)
 		{
-			printf("Fizz ");
+			printf("FizzBuzz ");
 		}
 		else if (n % 5 == 0)
 		{
 			printf("Buzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
 		}
 		else
 		{
