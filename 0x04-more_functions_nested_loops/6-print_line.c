d5f6fb003fbb68bf70d1eef7ee415560c0f29d88
Mrelshimy <mraafat.elsayed@gmail.com>
2023-08-17 03:44:56 -0700

    Adding file 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - prints n times of _
+ *@n : number of _ to print
+ *
+ * Return: On success 1.
+ */
+
+void print_line(int n)
+{
+	while (n >= 1)
+	{
+		_putchar(95);
+		n--;
+	}
+	_putchar(92);
+	_putchar(110);
+}
