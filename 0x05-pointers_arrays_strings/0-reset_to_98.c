f1bc1ecbccf0f504d3e6798c3bd6a22d94f7c503
Mrelshimy <mraafat.elsayed@gmail.com>
2023-08-20 23:16:11 -0700

    Adding file 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Set int value to 98
+ *@n : int to assing
+ *
+ * Discription : Function to  change int value using pointer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
