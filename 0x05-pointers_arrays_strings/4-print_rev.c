b8887a39664585979453a8ab71e5447693aa84b3
Mrelshimy <mraafat.elsayed@gmail.com>
2023-08-21 00:10:56 -0700

    Adding file 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ *@s : String
+ *
+ * Discription : Function to print string in reverse
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
+
