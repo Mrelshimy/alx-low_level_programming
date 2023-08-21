10327fc9918058e054417f16a0425fcde9e2994b
Mrelshimy <mraafat.elsayed@gmail.com>
2023-08-20 23:21:52 -0700

    Adding file 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - Set int value to 98
+ *@a : first int
+ *@b : second int
+ *
+ * Discription : Function to swap 2 ints values using pointer
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	*a = temp;
+	*a = *b;
+	*b = temp;
+}
+
