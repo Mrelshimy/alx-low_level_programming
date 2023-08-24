188a04b5de344e39a486fa1c67987c7a7886e99c
Mrelshimy <mraafat.elsayed@gmail.com>
2023-08-23 22:38:59 -0700

    Updating file 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,10 +11,11 @@ char *string_toupper(char *str)
 {
 	int i = 0;
 
-	for (i ; str[i] != '\0'; i++)
+	while (str[i] != '\0')
 	{
 		if (str[i] >= 97 && str[i] <= 122)
 			str[i] -= 32;
+		i++;
 	}
 
 	return (str);
