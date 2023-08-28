fe6149bf8f86c11e02378ae5f4cf09c498cbbecc
Mrelshimy <mraafat.elsayed@gmail.com>
2023-08-28 04:11:45 -0700

    Adding file 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *_strchr - strchr function code
+ *@s : String to search inside
+ *@c : char to find
+ *
+ *Description : a function to excute strchr .
+ *
+ *Return: pointer to byte of found char
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (s + i);
+
+		i++;
+	}
+
+	return (0);
+}
+
