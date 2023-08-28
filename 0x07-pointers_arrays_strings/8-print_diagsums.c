5c7ca563d66788ef5aa6797f6487509ccacc341d
Mrelshimy <mraafat.elsayed@gmail.com>
2023-08-28 12:23:32 -0700

    Addingfilw 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ * Return: Always 0 (Success)
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int d1 = 0;
+	int d2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		d1 += a[i];
+		d1 += a[size - i - 1];
+		a += size;
+	}
+
+	printf("%d", d1);
+	printf("%di\n", d2);
+}
+
