18001ecb9c8c8ec34f649c9f34109243245c5c9b
Mrelshimy <mraafat.elsayed@gmail.com>
2023-09-01 02:39:41 -0700

    Adding new proj argc argv directory

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *@argc : arg count
+ *@argv : arg vector
+ *
+ *Description: program to print it's name q/o the path
+ *
+ *Return: 0
+ */
+
+int main(argc, *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
