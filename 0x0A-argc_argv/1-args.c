bd956b25604922529a311206863a07f96537e58e
Mrelshimy <mraafat.elsayed@gmail.com>
2023-09-01 05:01:50 -0700

    Adding file 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,13 +5,13 @@
  *@argc : arg count
  *@argv : arg vector
  *
- *Description: program to print it's name q/o the path
+ *Description: program to prin number of it's i/p arguments
  *
  *Return: 0
  */
 
-int main(argc, *argv[])
+int main(int argc, char *argv[] __attribute__((__unused__)))
 {
-	printf("%s", argv[0]);
+	printf("%d\n", argc - 1);
 	return (0);
 }
