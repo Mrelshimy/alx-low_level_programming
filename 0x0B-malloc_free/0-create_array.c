5a3595a59e5e6a6f0f6a33b5dd9c55fde780f7f6
Mrelshimy <mraafat.elsayed@gmail.com>
2023-09-04 00:22:15 -0700

    updating file 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,14 +19,12 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	ptr = (char*) malloc(size * (sizeof(char)));
+	ptr = (char *) malloc(size * (sizeof(char)));
 
 	for (i = 0; i < size; i++)
 	{
-		*ptr = c;
-		ptr = ptr + 1;
+		ptr[i] = c;
 	}
 
 	return (ptr);
-	free(ptr);
 }
