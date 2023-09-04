e6ed57e5f930e7dc18ca620ce1d9e64657d23272
Mrelshimy <mraafat.elsayed@gmail.com>
2023-09-04 05:22:22 -0700

    Updating file 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,10 +19,10 @@ char *str_concat(char *s1, char *s2)
 	int size2 = 0;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "\0";
 
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "\0";
 
 	while (s1[size1] != '\0')
 		size1++;
@@ -30,7 +30,7 @@ char *str_concat(char *s1, char *s2)
 	while (s2[size2] != '\0')
 		size2++;
 
-	ptr = (char *) malloc(size1 + size2 - 1);
+	ptr = (char *) malloc(size1 + size2 + 1);
 
 	if (ptr == 0)
 		return (0);
@@ -41,5 +41,6 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; i < size2; i++)
 		ptr[i + size1] = s2[i];
 
+	ptr[size1 + size2] = '\0';
 	return (ptr);
 }
