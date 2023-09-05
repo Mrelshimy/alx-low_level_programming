b8b939f2ffe680b25e0e91c72654eaa7e1545a46
Mrelshimy <mraafat.elsayed@gmail.com>
2023-09-04 23:18:18 -0700

    Adding file 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * alloc_grid - C prog to return ptr to 2d array
+  * @height : height
+  * @width : width
+  *
+  * Discription : Program for return pointer to 2d array
+  *
+  *Return: pointer to new memory (Success) , 0 Fail
+  */
+
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int i;
+	int j;
+
+	if (height <= 0 || width <= 0)
+		return (0);
+
+	ptr = malloc(height * sizeof(int));
+
+	if (ptr == 0)
+		return (0);
+
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = malloc (width * sizeof(int));
+
+		if (*ptr == 0)
+			return (0);
+
+		for (j = 0; j < width; j++)
+			ptr[i][j] = 0;
+	}
+	 return(ptr);
+}
