26c5c663140923196e6acd5c984832636224f718
Mrelshimy <mraafat.elsayed@gmail.com>
2023-09-11 10:34:15 -0700

    Updating file 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,40 +1,7 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
-/**
-  *dog_t - C prog to add new dog data in struct dog
-  *@name : dog name
-  *@age : dog age
-  *@owner : owner
-  *
-  * Discription : Programto print struct dog element
-  *
-  * Return: NULL if fail
-  */
-
-char *_strcpy(char *dest, char *src);
-
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	dog_t *ndog;
-
-	ndog = malloc(sizeof(dog_t));
-	if (ndog == NULL)
-		return (0);
-	ndog->name = malloc(strlen(name));
-	if (ndog->name == NULL)
-		return (0);
-	ndog->owner = malloc(strlen(owner));
-	if (ndog->owner == NULL)
-		return (0);
-
-	_strcpy(ndog->name, name);
-	ndog->age = age;
-	_strcpy(ndog->owner, owner);
-	return(ndog);
-}
 
 /**
  * _strcpy  - copy a string to another
@@ -61,4 +28,44 @@ char *_strcpy(char *dest, char *src)
 		j++;
 	}
 	return (dest);
+}
+
+
+/**
+  *new_dog - C prog to add new dog data in struct dog
+  *@name : dog name
+  *@age : dog age
+  *@owner : owner
+  *
+  * Discription : Programto print struct dog element
+  *
+  * Return: NULL if fail
+  */
+
+
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ndog;
+	int i = 0, j = 0;
+
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
+
+	ndog = malloc(sizeof(dog_t));
+	if (ndog == NULL)
+		return (0);
+	ndog->name = malloc(i + 1);
+	if (ndog->name == NULL)
+		return (0);
+	ndog->owner = malloc(j + 1);
+	if (ndog->owner == NULL)
+		return (0);
+
+	_strcpy(ndog->name, name);
+	ndog->age = age;
+	_strcpy(ndog->owner, owner);
+	return (ndog);
 }
