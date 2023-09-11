a84bb42f47851e18354d0915812b8299175c3379
Mrelshimy <mraafat.elsayed@gmail.com>
2023-09-11 06:24:48 -0700

    Updating file 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,13 +6,22 @@
   * print_dog - C prog to print struct element
   *@d : ponter to struct
   *
-  * Discription : Programto print struct dog element 
+  * Discription : Programto print struct dog element
   *
   */
 
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d->name == NULL)
+		printf("(nil)");
+	else
+		printf("Name: %s\n", d->name);
+	if (d->age == NULL)
+		printf("(nil)");
+	else
+		printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("(nil)");
+	else
+		printf("Owner: %s\n", d->owner);
 }
