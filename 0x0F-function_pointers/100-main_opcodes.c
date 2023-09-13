d7112cf0a3ce4a0efe0f127d64ec4a271a4fe409
Mrelshimy <mraafat.elsayed@gmail.com>
2023-09-13 12:16:53 -0700

    Adding file 100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - entry point
+ *@argc: arguments count
+ *@argv: array of arguments
+ *
+ *Discription: Function to print its op
+ *
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, size;
+	char *p;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	size = atoi(argv[1]);
+	if (size < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	p = (char *)main;
+
+	for (i = 0; i <= size; i++)
+		printf("%02hhx\n", p[i]);
+	return (0);
+}
