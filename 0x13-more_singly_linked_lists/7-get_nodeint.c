b7ba9de2b914faf4d926f52fcd17fffb70b2dfde
Mrelshimy <mraafat.elsayed@gmail.com>
2023-09-25 02:35:12 -0700

    Updating file 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,16 +14,20 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *nnode, *hold;
 	unsigned int i = 0;
+	hold = head;
 
 	nnode = malloc(sizeof(listint_t));
 	if (nnode == NULL)
 		return (NULL);
 
 	while (i < index)
+	{
 		hold = hold->next;
+		i++;
+	}
 
 	nnode->next = hold->next;
-	hold->nect = nnode;
+	hold->next = nnode;
 
 	return (nnode);
 }
