36b8428da1220fed539567ff79952933aad733f6
Mrelshimy <mraafat.elsayed@gmail.com>
2023-09-26 12:16:05 -0700

    Updating 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,12 +11,14 @@
 
 int pop_listint(listint_t **head)
 {
-	listint_t *hold = (*head)->next;
-	int n = (*head)->n;
+	listint_t *hold;
+	int n;
 
 	if (*head == NULL || head == NULL)
 		return (0);
 
+	n = (*head)->n;
+	hold = (*head)->next;
 	free(*head);
 	*head = hold;
 	return (n);
