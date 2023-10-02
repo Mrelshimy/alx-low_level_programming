2d7cd8f4e2bfb5045cdfaddf019d008fda08ee1e
Mrelshimy <mraafat.elsayed@gmail.com>
2023-10-02 11:21:28 -0700

    Adding file 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,37 +18,36 @@ int main(int argc, char *argv[])
 	char readsize[BUFF_SIZE];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	{	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
 	ffp = open(argv[1], O_RDONLY);
 	if (ffp == -1)
-	{
-		dprintf(STDERR_FILENO, "ERROR: Can't read from file %s\n", argv[1]);
+	{	dprintf(STDERR_FILENO, "ERROR: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	tfp = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (tfp == -1)
-	{
-		dprintf(STDERR_FILENO, "ERROR: Can't write to %s\n", argv[2]);
+	{	dprintf(STDERR_FILENO, "ERROR: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	while ((rt = read(ffp, readsize, BUFF_SIZE)) > 0)
 		if(write(tfp, readsize, rt) != rt)
-		{
-			dprintf(STDERR_FILENO, "ERROR: Can't write to %s\n", argv[2]);
+		{	dprintf(STDERR_FILENO, "ERROR: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
+	if (rt == -1)
+	{	dprintf(STDERR_FILENO, "ERROR: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
 	if (close(ffp) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ffp);
+	{	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ffp);
 		exit(100);
 	}
 	if (close(tfp) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", tfp);
+	{	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", tfp);
 		exit(100);
 	}
 	return (0);
