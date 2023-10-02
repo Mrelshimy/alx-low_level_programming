403fc0d49d7f5ee3f34189113e41ad31011cf1d3
Mrelshimy <mraafat.elsayed@gmail.com>
2023-10-02 01:10:41 -0700

    Adding 0x15 + files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *read_textfile - read and print text file
+ *@filename : pointer to file name
+ *@letters : letters to read
+ *
+ *Discription: Function te read file and print it to STDOUT
+ *
+ *Return: actual number of letters , 0 if fail
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *readsize;
+	int fp = open(filename, O_RDONLY);
+	int textout;
+
+	if (filename == NULL || !letters)
+		return (0);
+
+	if (fp == -1)
+		return (0);
+
+	readsize = malloc(letters);
+	textout = read(fp, readsize, letters);
+	textout = write(STDOUT_FILENO, readsize, textout);
+
+	if (textout == -1)
+		return (0);
+
+	free(readsize);
+	close(fp);
+	return (textout);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+README for Alx new project 0x15-file_io
