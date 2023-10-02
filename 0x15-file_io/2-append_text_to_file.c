b4dffda5b15a02c48ee833f7edf982da6965f5a1
Mrelshimy <mraafat.elsayed@gmail.com>
2023-10-02 05:07:48 -0700

    Updating file 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,16 +12,19 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fp = open(filename, O_WRONLY, O_APPEND);
+	int fp;
 	int l;
 
-	if (fp == -1)
-		return (-1);
 	if (filename == NULL)
+		return (-1);
+
+	fp = open(filename, O_WRONLY | O_APPEND);
+	if (fp == -1)
 		return (-1);
 
 	if (text_content != NULL)
 		l = strlen(text_content);
+
 	if (write(fp, text_content, l) != -1)
 		return (1);
 	else
