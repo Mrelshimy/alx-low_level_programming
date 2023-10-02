54870a1978c187b113c929bfe88fc91c4f1ccbdb
Mrelshimy <mraafat.elsayed@gmail.com>
2023-10-02 04:54:46 -0700

    Updating file 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,36 @@
 #include "main.h"
 
-
 /**
- * create_file - Function to create file.
- * @filename: pointer to filename
- * @text_content: pointer to text content
+ *create_file - read and print text file
+ *@filename : pointer to file name
+ *@text_content : tect content
  *
- * Return: 1 success -1 fail.
+ *Discription: Function te read file and print it to STDOUT
  *
+ *Return: 1 success , -1 if fail
  */
 
 int create_file(const char *filename, char *text_content)
 {
 	int fp;
-	int twrite;
 	int l = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (l = 0; text_content[l];)
-			l++;
-	}
-	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	twrite = write(fp, text_content, l);
-
-	if (fp == -1 || twrite == -1)
+	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	
+	if (fp == -1)
 		return (-1);
+	
+	while (text_content[l] != '\0')
+		l++;
+	
+	if (write(fp, text_content, l) != -1)
+		return (1);
+	else
+		return (-1);
+
 	close(fp);
 	return (1);
 }
