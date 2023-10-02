71e6d1add683919455e100cc28785adb91d56b97
Mrelshimy <mraafat.elsayed@gmail.com>
2023-10-02 11:15:45 -0700

    Adding file 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,24 +24,23 @@ int main(int argc, char *argv[])
 	}
 
 	ffp = open(argv[1], O_RDONLY);
-	rt = read(ffp, readsize, BUFF_SIZE);
+	if (ffp == -1)
+	{
+		eprintf(STDERR_FILENO, "ERROR: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	tfp = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-	do {
-		if (ffp == -1 || rt == -1)
-		{
-			dprintf(STDERR_FILENO, "ERROR: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-		wt = write(tfp, readsize, rt);
-		if (tfp == -1 || wt == -1)
+	if (tfp == -1)
+	{
+		dprintf(STDERR_FILENO, "ERROR: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while ((rt = read(ffp, readsize, BUFF_SIZE)) > 0)
+		if(write(tfp, readsize, rt) != rt)
 		{
 			dprintf(STDERR_FILENO, "ERROR: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
-		rt = read(ffp, readsize, BUFF_SIZE);
-		tfp = open(argv[2], O_WRONLY | O_APPEND);
-	} while (rt > 0);
 	if (close(ffp) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ffp);
