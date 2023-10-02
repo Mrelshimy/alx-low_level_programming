e50d6993b2bb62b0360a285b6316489195cfbbae
Mrelshimy <mraafat.elsayed@gmail.com>
2023-10-02 11:57:27 -0700

    Updatinf file 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,56 +1,88 @@
 #include "main.h"
-#define BUFF_SIZE 1024
+
+char *buff(char *filename);
+void closef(int fp);
 
 /**
- *main - copies file to another filw
- *@argc : number of arguments
- *@argv : array of arguments
+ * buff - mallocs 1024 bytes.
+ * @filename : filename
  *
- *Discription: Function to copy file to another
- *
- *Return: 0 on success
+ * Return: pointer to buffer.
  */
-
-int main(int argc, char *argv[])
+char *buff(char *filename)
 {
-	int tfp = 0, ffp = 0;
-	int rt;
-	char readsize[BUFF_SIZE];
+	char *nbuff;
 
-	if (argc != 3)
-	{	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	ffp = open(argv[1], O_RDONLY);
-	if (ffp == -1)
-	{	dprintf(STDERR_FILENO, "ERROR: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	tfp = open(argv[2], O_WRONLY | O_WRONLY | O_TRUNC, 0664);
-	if (tfp == -1)
-	{	dprintf(STDERR_FILENO, "ERROR: Can't write to %s\n", argv[2]);
+	nbuff = malloc(1024);
+	if (nbuff == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
 		exit(99);
 	}
-	while ((rt = read(ffp, readsize, BUFF_SIZE)) > 0)
-		if (write(tfp, readsize, rt) != rt)
-		{	dprintf(STDERR_FILENO, "ERROR: Can't write to %s\n", argv[2]);
+	return (nbuff);
+}
+
+/**
+ * closef - closes file
+ * @fp: file descriptor
+ */
+void closef(int fp)
+{
+	int ch;
+
+	ch = close(fp);
+	if (ch == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copies afile to another
+ * @argc: arguments count
+ * @argv: array of args
+ *
+ * description  copies a file to another
+ *
+ * Return: 0 success.
+ */
+int main(int argc, char *argv[])
+{
+	int ffp, tfp;
+	int rt, wt;
+	char *readsize;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	readsize = buff(argv[2]);
+	ffp = open(argv[1], O_RDONLY);
+	rt = read(ffp, readsize, 1024);
+	tfp = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (ffp == -1 || rt == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			free(readsize);
+			exit(98);
+		}
+		wt = write(tfp, readsize, rt);
+		if (tfp == -1 || wt == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(readsize);
 			exit(99);
 		}
-	if (rt == -1)
-	{	dprintf(STDERR_FILENO, "ERROR: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	ffp = close(ffp);
-	tfp = close(tfp);
-	if (ffp)
-	{	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ffp);
-		exit(100);
-	}
-	if (tfp)
-	{	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", tfp);
-		exit(100);
-	}
+		rt = read(ffp, readsize, 1024);
+		tfp = open(argv[2], O_WRONLY | O_APPEND);
+	} while (rt > 0);
+	free(readsize);
+	closef(ffp);
+	closef(tfp);
 	return (0);
 }
+
