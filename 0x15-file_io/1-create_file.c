edbc40333bb1aa11facef1e0afc8b5a599d22f45
Mrelshimy <mraafat.elsayed@gmail.com>
2023-10-02 04:43:25 -0700

    Updating file 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  * @text_content: pointer to text content
  *
  * Return: 1 success -1 fail.
- * 
+ *
  */
 
 int create_file(const char *filename, char *text_content)
@@ -24,11 +24,10 @@ int create_file(const char *filename, char *text_content)
 		for (l = 0; text_content[l];)
 			l++;
 	}
-	
 	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	twrite = write(fp, text_content, l);
 
-	if (fp== -1 || twrite == -1)
+	if (fp == -1 || twrite == -1)
 		return (-1);
 	close(fp);
 	return (1);
