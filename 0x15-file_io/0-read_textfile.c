de50c0ebd0877942e9e16ce4d9f9fad65dc20287
Mrelshimy <mraafat.elsayed@gmail.com>
2023-10-02 02:10:22 -0700

    Updating file 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,8 +13,9 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *readsize;
-	int fp = open(filename, O_RDONLY);
-	int textout;
+	ssize_t textread
+	ssize_t fp = open(filename, O_RDONLY);
+	ssize_t textout;
 
 	if (filename == NULL || !letters)
 		return (0);
@@ -23,8 +24,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	readsize = malloc(letters);
-	textout = read(fp, readsize, letters);
-	textout = write(STDOUT_FILENO, readsize, textout);
+	textread = read(fp, readsize, letters);
+	textout = write(STDOUT_FILENO, readsize, textread);
 
 	if (textout == -1)
 		return (0);
