3edad1df327cfb62d0bfec9c9377c1eaefdbbd62
Mrelshimy <mraafat.elsayed@gmail.com>
2023-10-02 02:24:25 -0700

    Updating file 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,13 +12,15 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	int fp;
 	int l;
 
-	if (fp == -1)
-		return (-1);
 	if (filename == NULL)
 		return (-1);
+
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fp == -1)
+		return (-1); 
 
 	l = strlen(text_content);
 
