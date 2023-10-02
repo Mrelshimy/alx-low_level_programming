97981581366722d986b0abf0fc24f5466a81d9b5
Mrelshimy <mraafat.elsayed@gmail.com>
2023-10-02 01:50:05 -0700

    Adding files 1 , 2

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *create_file - read and print text file
+ *@filename : pointer to file name
+ *@text_content : tect content
+ *
+ *Discription: Function te read file and print it to STDOUT
+ *
+ *Return: 1 success , -1 if fail
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	int l;
+
+	if (fp == -1)
+		return (-1);
+	if (filename == NULL)
+		return (-1);
+
+	l = strlen(text_content);
+
+	if (write(fp, text_content, l) != -1)
+		return (1);
+	else
+		return (-1);
+
+	close(fp);
+	return (1);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *append_text_to_file - read and print text file
+ *@filename : pointer to file name
+ *@text_content : tect content to append
+ *
+ *Discription: Function to append text to file
+ *
+ *Return: 1 success , -1 if fail
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fp = open(filename, O_WRONLY, O_APPEND);
+	int l;
+
+	if (fp == -1)
+		return (-1);
+	if (filename == NULL)
+		return (-1);
+
+	l = strlen(text_content);
+	if (write(fp, text_content, l) != -1)
+		return (1);
+	else
+		return (-1);
+
+	close(fp);
+	return (1);
+}
