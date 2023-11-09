5197c05e0f8ccdb55a843afb100b3a164dd78cde
Mrelshimy <mraafat.elsayed@gmail.com>
2023-11-09 09:40:59 -0800

    Adding file 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -13,7 +13,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *tmp = *head;
 	unsigned int n = 0;
 
-	if (*head == NULL || (*head)->next == NULL)
+	if (*head == NULL)
 		return (-1);
 
 	if (index == 0)
