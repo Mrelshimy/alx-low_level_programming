65a3119c4e350a72fca841626013e1cbd68b5331
Mrelshimy <mraafat.elsayed@gmail.com>
2023-11-13 11:41:37 -0800

    opdating file 101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P --output-document=$HOME/gigamlns.so https://github.com/Mrelshimy/alx-low_level_programming/raw/main/0x18-dynamic_libraries/gigamlns.so
-export LD_PRELOAD=$HOME/lib101-makemewin.so
+wget -P /tmp https://github.com/Mrelshimy/alx-low_level_programming/raw/main/0x18-dynamic_libraries/gigamlns.so
+export LD_PRELOAD=/tmp/gigamlns.so
