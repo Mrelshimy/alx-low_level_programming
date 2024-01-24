0fc00a441a1874168e033171b543e4f04e4e5fd7
Mrelshimy <mraafat.elsayed@gmail.com>
2024-01-24 21:12:03 +0200

    Adding file 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,8 +6,8 @@ def island_perimeter(grid):
     """Island_perimeter function"""
     count = 0
 
-    for i in range(1, len(grid) - 1):
-        for j in range(1, len(grid[i]) - 1):
+    for i in range(1, len(grid)):
+        for j in range(1, len(grid[i])):
             if grid[i][j] == 1:
                 count += 4
                 if grid[i - 1][j] == 1:
